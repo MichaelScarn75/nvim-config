aaffd69a803ada347849649d4e2c731834173c66
disconnect75 <wiagilt75@gmail.com>
2025-09-21 23:01:55 +0800

    Modified telescope.lua. gr now targets word under cursor for grep string. While gs is for general grep string.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,19 +2,11 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>tff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>tgf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>tlg', function()
-    local text = vim.fn.getreg("v")
-    if text ~= nil and text ~= "" then
-        builtin.grep_string({ search = text })
-    else
-        builtin.live_grep({ search = vim.fn.expand("<cword>") });
-    end
+    builtin.live_grep({ search = vim.fn.expand("<cword>") });
 end)
 vim.keymap.set('n', 'gs', function()
-    local text = vim.fn.getreg("v")
-    print(text);
-    if text ~= nil and text ~= "" then
-        builtin.grep_string({ search = text })
-    else
-        builtin.grep_string({ search = vim.fn.expand("<cword>") });
-    end
+    builtin.grep_string({ search = "" })
+end)
+vim.keymap.set('n', 'gr', function()
+    builtin.grep_string({ search = vim.fn.expand("<cword>") });
 end)
